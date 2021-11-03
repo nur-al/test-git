e943913908dde7f7f40c9fb8d88c9b137ff82e6f
Nuriev Albert <albertnuriev84@gmail.com>
2021-11-03 15:54:16 +0500

    Initial parenthesis.py

---
+++ parenthesis.py
@@ -0,0 +1,16 @@
+def valid_parenthesis(string):
+	temp_list = []
+	for i in string:
+		if i == '(':
+			temp_list.append(i)
+		elif i == ')':
+			if len(temp_list) == 0:
+				return False
+			elif temp_list[-1] == '(':
+				temp_list.pop()
+			else:
+				return False
+	if len(temp_list) == 0:
+		return True
+	else:
+		return False
